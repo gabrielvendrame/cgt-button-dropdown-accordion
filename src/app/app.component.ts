426ab53ed3e118a1d021a77f68e063153d6b98ee
Gabriel Vendrame <gabriel@Gabriels-iMac.local>
2021-03-01 17:51:37 +0100

    Working on svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DropdownContentButton } from './components/button-dropdown-accordion/button-dropdown-accordion.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  isHidden = true;
+export class AppComponent implements OnInit {
+  buttons: DropdownContentButton[] = [];
 
-  toggleAccordion(): void {
-    this.isHidden = !this.isHidden;
+  ngOnInit(): void {
+    this.buttons.push({
+      title: 'Ciao',
+      buttonColor: 'red',
+      textColor: 'white',
+      icon: 'alarm-fill',
+      action: (() => console.log(222))
+    }, {
+      title: 'Are',
+      buttonColor: 'blue',
+      textColor: 'red',
+      icon: 'align-middle',
+      action: (() => console.log(44))
+    });
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
 import { AppComponent } from './app.component';
+import { ButtonDropdownAccordionComponent } from './components/button-dropdown-accordion/button-dropdown-accordion.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonDropdownAccordionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxBootstrapIconsModule.pick(allIcons),
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
